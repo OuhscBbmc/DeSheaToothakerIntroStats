3987332a8f12a2e180cf58d4c450056d000a8b6d
Will Beasley <wibeasley@hotmail.com>
2014-01-14 19:22:52 -0600

    Working on fonts and adding data

---
+++ Chapter03/Chapter03.R
@@ -0,0 +1,56 @@
+rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run.
+
+#####################################
+## @knitr LoadPackages
+require(knitr)
+require(ggplot2)
+
+#####################################
+## @knitr DeclareGlobals
+source("./CommonCode/BookTheme.R")
+chapterTheme <- BookTheme +
+  theme(axis.title.y=element_blank())
+
+#####################################
+## @knitr LoadDatasets
+# 'ds' stands for 'datasets'
+dsSkewZero <- data.frame(Systolic=c(112, 112, 114, 115, 118, 121, 122, 124, 124))
+dsSkewPositive <- data.frame(Systolic=c(65, 65, 66, 67, 67, 68, 70, 70, 79))
+dsSkewNegative <- data.frame(Systolic=c(60, 60, 70, 70, 72, 74, 74, 75, 76))
+
+#####################################
+## @knitr TweakDatasets
+# (This code chunk is intentionally empty.)
+
+#####################################
+## @knitr Figure02_01
+ggplot(dsSkewZero, aes(x=Systolic)) +
+  geom_dotplot(binwidth=1, fill="darkgreen", color=NA, method="dotdensity") +
+  scale_x_continuous(breaks=seq(from=min(dsSkewZero$Systolic), to=max(dsSkewZero$Systolic), by=1)) +
+  scale_y_continuous(breaks=NULL) +
+  chapterTheme +
+  labs(x="Systolic Blood Pressure")
+
+#####################################
+## @knitr Figure02_02
+ggplot(dsSkewPositive, aes(x=Systolic)) +
+  geom_dotplot(binwidth=1, fill="lightblue", color=NA, method="dotdensity") +
+  scale_x_continuous(breaks=seq(from=min(dsSkewPositive$Systolic), to=max(dsSkewPositive$Systolic), by=1)) +
+  scale_y_continuous(breaks=NULL) +
+  chapterTheme +
+  labs(x="Systolic Blood Pressure")
+
+#####################################
+## @knitr Figure02_03
+ggplot(dsSkewNegative, aes(x=Systolic)) +
+  geom_dotplot(binwidth=1, fill="lightblue3", color=NA, method="dotdensity") +
+  scale_x_continuous(breaks=seq(from=min(dsSkewNegative$Systolic), to=max(dsSkewNegative$Systolic), by=1)) +
+  scale_y_continuous(breaks=NULL) +
+  chapterTheme +
+  labs(x="Systolic Blood Pressure")
+
+#####################################
+# TODO: Questions for Lise:
+# 1. Are the colors matched closely enough, or do they need to be more exact?
+# 2. Drop or keep x-axis title?
+# 3. Use dots or squares?
--- CommonCode/BookTheme.R
+++ CommonCode/BookTheme.R
@@ -1,4 +1,24 @@
+#For fonts, see Chang (2013) Recipe 14.6.  Install ghostscript (http://www.ghostscript.com/download/gsdnld.html),
+# before installing the `extrafont` package.
+#Run the following three lines of code once per machine (not once per session).
+# install.packages("extrafont")
+# require(extrafont) 
+# extrafont::font_import()
+# extrafont::fonts() #This just lists the available fonts for you to read
+
+require(extrafont) 
 require(ggplot2)
 
+# extrafont::fonttable()
+extrafont::loadfonts() #Run this once per session.
+Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.10/bin/gswin64c.exe")
+
 # Documentation for modifiable theme elements can be found at http://docs.ggplot2.org/current/theme.html
-BookTheme <- theme_bw()
+BookTheme <- theme_bw() +
+  theme(text = element_text(family="Times New Roman"))
+#   theme(text = element_text(family="Times New Roman"))
+
+#Internal notes:
+# * The “Pre-Press Manager” said the dimensions of the images cannot exceed these dimensions:  33 picas wide x 51 picas tall. (5.5" x 8.5")
+
+# list.files(system.file("enc", package="grDevices"))

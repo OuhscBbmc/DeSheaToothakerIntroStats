fa6d4682ea235a123b851835a617beed2dd1c775
Will Beasley <wibeasley@hotmail.com>
2014-03-08 16:46:06 -0600

    Figure 13-02 complete (ie, theoretical R)

--- Chapter13/Chapter13.R
+++ Chapter13/Chapter13.R
@@ -61,246 +61,48 @@ gObesity <- ggplot(dsObesity, aes(x=FoodHardshipRate, y=ObesityRate)) +
 gObesity
 
 #####################################
-## @knitr Figure12_02
+## @knitr Figure13_02
+intervalWidth <- .001
+rPrior <- 0
+nPrior <- 51
+precPrior <- nPrior - 3
+zPrior <- atanh(rPrior)
+zCrit <- qnorm(p=c(.025, .975), mean=zPrior, sd=1/sqrt(precPrior))
+rCrit <- tanh(zCrit)
 
+Gauss <- function( fixed, rho, n ) {
+  zObs <- atanh(fixed)
+  zHyp <- atanh(rho)
+  standardError <- 1/sqrt(n - 3)
+  return( dnorm(x=zObs, mean=zHyp, sd=standardError) )
+}
 
+dsRho <- data.frame(ZPossible = seq(from=-3+intervalWidth, to=3-intervalWidth, by=intervalWidth))
+dsRho$RhoPossible <- tanh(dsRho$ZPossible)
+dsRho$PriorR <- Gauss(fixed=rPrior, rho=dsRho$RhoPossible, n=nPrior)
+dsRho$TailLower <- (dsRho$RhoPossible <= rCrit[1])
+dsRho$TailUpper <- (rCrit[2] <= dsRho$RhoPossible)
 
-#####################################
-## @knitr Figure12_03
-AnovaSingleScenario(scenarioID=2, scenarioName="Scenario 12-B")
-
-#####################################
-## @knitr Figure12_04
-AnovaSingleScenario(scenarioID=3, scenarioName="Scenario 12-C")
-
-#####################################
-## @knitr Figure12_05
-ggplot(dsFeed, aes(x=Sleep, color=Feeding, fill=Feeding)) +
-  geom_histogram(binwidth=10)  +
-  geom_vline(aes(xintercept=M), data=dsScenarioFeeding, size=2, color="#55555544")  +
-  geom_text(data=dsScenarioFeeding, aes(x=M, y=Inf, label=LabelM), color="gray40", vjust=1.2, hjust=1.1, size=3, parse=TRUE) +
-  geom_text(data=dsScenarioFeeding, aes(x=M, y=Inf, label=LabelSD), color="gray40", vjust=1.2, hjust=-.1, size=3, parse=TRUE) +
-  scale_x_continuous(expand=c(0, 0)) +
-  scale_y_continuous(limits=c(0, 4.8), expand=c(0, 0)) + #Coordinate this y limit with the previous anova function.
-  scale_color_manual(values=paletteFeeding) +
-  scale_fill_manual(values=paletteFeedingLight) +
-  coord_cartesian(xlim=rangeSleep) +
-  facet_grid(Feeding ~ Scenario) +
-  chapterTheme +
-  theme(legend.position="none") +
-  labs(x="Minutes of sleep in 24 hours", y="Frequency", title=NULL) 
-
-#####################################
-## @knitr Figure12_06
-#bb5210    #eb6c1d    #fe8011    
-#fe9e4c    #ffffff    #e5e5e5    
-#c6c6c6    #919191    #97d2f6    
-#63bdf2    #1e96e0    #0c65bf    
-
-# fpaletteFeedingDark <- c("#bb5210", "#0c65bf")# http://colrd.com/palette/23379/
-fPaletteDark <- c("#eb6c1daa", "#1e96e0aa")# http://colrd.com/palette/23379/
-fPaletteLight <- c("#fe9e4c", "#97d2f6")# http://colrd.com/palette/23379/
-
-f1DfModel <- 2; f1DfError <- 30
-f2DfModel <- 5; f2DfError <- 96
-
-f1 <- function( x ) { return( df(x, df1=f1DfModel, df2=f1DfError) ) }
-f2 <- function( x ) { return( df(x, df1=f2DfModel, df2=f2DfError) ) }
-
-ggplot(data.frame(f=c(0, 4.5)), aes(x=f)) +
-  stat_function(fun=f1, geom="area", fill=fPaletteLight[1], alpha=.3, n=calculatedPointCount) +
-  stat_function(fun=f2, geom="area", fill=fPaletteLight[2], alpha=.3, n=calculatedPointCount) +
-  stat_function(fun=f1, n=calculatedPointCount, color=fPaletteDark[1], size=2) +
-  stat_function(fun=f2, n=calculatedPointCount, color=fPaletteDark[2], size=2) +
-  annotate(geom="text", x=.1, y=f1(.1), label=paste0("italic(F)(", f1DfModel, ", ", f1DfError,")"), hjust=-.15, parse=TRUE, color=fPaletteDark[1]) +
-  annotate(geom="text", x=1, y=f2(1), label=paste0("italic(F)(", f2DfModel, ", ", f2DfError,")"), hjust=-.15, parse=TRUE, color=fPaletteDark[2]) +
-  scale_x_continuous(expand=c(0,0)) +
-  scale_y_continuous(breaks=NULL, expand=c(0,0)) +
-  expand_limits(y=f1(0) * 1.05) +
-  chapterTheme +
-  labs(x=expression(italic(F)), y=NULL)
-
-#####################################
-## @knitr Figure12_08
-PaletteCritical <- c("#544A8C", "#ce2b18", "#F37615") #Adapted from http://colrd.com/palette/17511/ (I made the purple lighter and the orange darker)
-
-f3DfModel <- 3; f3DfError <- 80
-f3 <- function( x ) { return( df(x, df1=f3DfModel, df2=f3DfError) ) }
-criticalF05 <- qf(p=.95, df1=f3DfModel, df2=f3DfError)
-criticalF01 <- qf(p=.99, df1=f3DfModel, df2=f3DfError)
-
-###
-### Together
-###
-grid.newpage()
-gCritical <- ggplot(data.frame(f=c(0, 4.5)), aes(x=f)) +
-  annotate("segment", x=criticalF05, xend=criticalF05, y=0, yend=Inf, color=PaletteCritical[2]) +
-  annotate("segment", x=criticalF01, xend=criticalF01, y=0, yend=Inf, color=PaletteCritical[3]) +  
-  stat_function(fun=LimitRange(f3, criticalF05, Inf), geom="area", fill=PaletteCritical[2], alpha=1, n=calculatedPointCount) +  
-  stat_function(fun=LimitRange(f3, criticalF01, Inf), geom="area", fill=PaletteCritical[3], alpha=1, n=calculatedPointCount) +
-  stat_function(fun=f3, n=calculatedPointCount, color=PaletteCritical[1], size=.5) +
-  annotate(geom="text", x=criticalF05+.05, y=f3(criticalF05)+.20, label="alpha==phantom(0)", hjust=-.15, vjust=-.15, parse=TRUE, color=PaletteCritical[2]) +
-  annotate(geom="text", x=criticalF05+.05, y=f3(criticalF05)+.20, label=".05", hjust=-.15, vjust=1.15, parse=F, color=PaletteCritical[2]) +
+cat("Lise, would you like to keep the critical values, or hide them?")
+gCriticalR <- ggplot(dsRho, aes(x=RhoPossible, y=PriorR)) + #, fill=TailLower
+  annotate("segment", x=rCrit, xend=rCrit, y=0, yend=Inf, color=PaletteCritical[2]) +
+  geom_area(data=dsRho[dsRho$TailLower, ], aes(x=RhoPossible, y=PriorR), fill=PaletteCritical[2]) +
+  geom_area(data=dsRho[dsRho$TailUpper, ], aes(x=RhoPossible, y=PriorR), fill=PaletteCritical[2]) +
+  geom_line(color=PaletteCritical[1]) +
   
-  annotate(geom="text", x=criticalF01+.05, y=f3(criticalF01)+.10, label="alpha==phantom(0)", hjust=-.15, vjust=-.15, parse=TRUE, color=PaletteCritical[3]) +
-  annotate(geom="text", x=criticalF01+.05, y=f3(criticalF01)+.10, label=".01", hjust=-.15, vjust=1.15, parse=F, color=PaletteCritical[3]) +
-
-  annotate("segment", x=criticalF05, xend=4.4, y=f3(criticalF05)+.15, yend=f3(criticalF05)+.15, color=PaletteCritical[2], arrow=arrow(length=grid::unit(0.2, "cm"), type="open"), lineend="round", linetype="F2") +
-  annotate("segment", x=criticalF01, xend=4.4, y=f3(criticalF01)+.05, yend=f3(criticalF01)+.05, color=PaletteCritical[3], arrow=arrow(length=grid::unit(0.2, "cm"), type="open"), lineend="round", linetype="F2") +
+  annotate(geom="text", x=rCrit[1]-.09, y=.8, label="alpha/2==phantom(0)", hjust=.5, vjust=-.05, parse=TRUE, color=PaletteCritical[2]) +
+  annotate(geom="text", x=rCrit[1]-.09, y=.8, label=".025", hjust=.5, vjust=1.05, parse=F, color=PaletteCritical[2]) +
+  annotate(geom="text", x=rCrit[1], y=0, label=round(rCrit[1],2), hjust=.5, vjust=1.2, parse=F, color=PaletteCritical[2], size=5) +
   
-  annotate(geom="text", x=criticalF05, y=0, label=round(criticalF05, 2), hjust=.5, vjust=1.2, color=PaletteCritical[2], size=5) +
-  annotate(geom="text", x=criticalF01, y=0, label=round(criticalF01, 2), hjust=.5, vjust=1.2, color=PaletteCritical[3], size=5) +
-  scale_x_continuous(expand=c(0,0), breaks=0:4, labels=c(0, 1, 2, 3, "")) +
-  scale_y_continuous(breaks=NULL, expand=c(0,0)) +
-  expand_limits(y=f3(.4) * 1.05) +
-  chapterTheme +
-  theme(axis.text = element_text(colour="gray60")) + #Lighten so the critical values aren't interfered with
-  labs(x=expression(italic(F)), y=NULL)
-
-DrawWithoutPanelClipping(gCritical)
-
-###
-### Just .05
-###
-grid.newpage()
-gCritical <- ggplot(data.frame(f=c(0, 4.5)), aes(x=f)) +
-  annotate("segment", x=criticalF05, xend=criticalF05, y=0, yend=Inf, color=PaletteCritical[2]) +
-  stat_function(fun=LimitRange(f3, criticalF05, Inf), geom="area", fill=PaletteCritical[2], alpha=1, n=calculatedPointCount) +  
-  stat_function(fun=f3, n=calculatedPointCount, color=PaletteCritical[1], size=.5) +
-  annotate(geom="text", x=3, y=f3(criticalF05)+.05, label="alpha==phantom(0)", hjust=-.15, vjust=-.15, parse=TRUE, color=PaletteCritical[2]) +
-  annotate(geom="text", x=3, y=f3(criticalF05)+.05, label=".05", hjust=-.15, vjust=1.15, parse=F, color=PaletteCritical[2]) +
-  annotate(geom="text", x=criticalF05, y=0, label=round(criticalF05, 2), hjust=.5, vjust=1.2, color=PaletteCritical[2], size=5) +
-  scale_x_continuous(expand=c(0,0), breaks=0:4, labels=c(0, 1, 2, 3, "")) +
-  scale_y_continuous(breaks=NULL, expand=c(0,0)) +
-  expand_limits(y=f3(.4) * 1.05) +
-  chapterTheme +
-  theme(axis.text = element_text(colour="gray60")) + #Lighten so the critical values aren't interfered with
-  labs(x=expression(italic(F)), y=NULL)
-
-DrawWithoutPanelClipping(gCritical)
-
-###
-### Just .01
-###
-grid.newpage()
-gCritical <- ggplot(data.frame(f=c(0, 4.5)), aes(x=f)) +
-  annotate("segment", x=criticalF01, xend=criticalF01, y=0, yend=Inf, color=PaletteCritical[3]) +
-  stat_function(fun=LimitRange(f3, criticalF01, Inf), geom="area", fill=PaletteCritical[3], alpha=1, n=calculatedPointCount) +
-  stat_function(fun=f3, n=calculatedPointCount, color=PaletteCritical[1], size=.5) +
-  annotate(geom="text", x=4.1, y=f3(criticalF01)+.05, label="alpha==phantom(0)", hjust=-.15, vjust=-.15, parse=TRUE, color=PaletteCritical[3]) +
-  annotate(geom="text", x=4.1, y=f3(criticalF01)+.05, label=".01", hjust=-.15, vjust=1.15, parse=F, color=PaletteCritical[3]) +
-  annotate(geom="text", x=criticalF01, y=0, label=round(criticalF01, 2), hjust=.5, vjust=1.2, color=PaletteCritical[3], size=5) +
-  scale_x_continuous(expand=c(0,0), breaks=0:4, labels=c(0, 1, 2, 3, "")) +
-  scale_y_continuous(breaks=NULL, expand=c(0,0)) +
-  expand_limits(y=f3(.4) * 1.05) +
-  chapterTheme +
-  theme(axis.text = element_text(colour="gray60")) + #Lighten so the critical values aren't interfered with
-  labs(x=expression(italic(F)), y=NULL)
-
-DrawWithoutPanelClipping(gCritical)
-#####################################
-## @knitr Figure12_09
-set.seed(891) #Set the random number generator seed so the jitters are consistent
-ggplot(dsCry, aes(x=1, y=CryingDuration, color=Group, fill=Group)) +
-  geom_boxplot(width=.8, outlier.colour=NA) +
-  geom_point(position=position_jitter(w=0.2, h=0), size=4, shape=21) +
-  stat_summary(fun.y="mean", geom="point", shape=23, size=7, fill="#FFFFFFCC",  na.rm=T) + #See Chang (2013), Recipe 6.8.
-#   geom_text(aes(x=Inf, y=M, label=LabelM), color="gray40", vjust=1.2, hjust=1.1, size=3, parse=TRUE) +
-  #   geom_text(data=dsScenarioFeeding[dsScenarioFeeding$ScenarioID==2, ], aes(x=Inf, y=M, label=LabelM), color="gray40", vjust=1.2, hjust=1.1, size=3, parse=TRUE) +
-  #   geom_text(data=dsScenarioFeeding[dsScenarioFeeding$ScenarioID==2, ], aes(x=Inf, y=M, label=LabelSD), color="gray40", vjust=1.2, hjust=-.1, size=3, parse=TRUE) +
-  #   geom_hline(aes(yintercept=M), data=dsScenarioFeeding) +
-  scale_x_continuous(breaks=10) +
-  scale_color_manual(values=paletteCryBox) +
-  scale_fill_manual(values=paletteCryBoxLight) +
-  facet_grid(Group ~ .) +
-  coord_flip() + #xlim=c(.65, 1.4)) + #ylim=rangeSleep
-  chapterTheme +
-  theme(legend.position="none") +
-  labs(x=NULL, y="Crying Duration")
-
-#####################################
-## @knitr Figure12_10
-cryMeanOverall <- mean(dsCry$CryingDuration)
-cryMeanControl <- mean(dsCry[dsCry$GroupID==3, "CryingDuration"])
-cryMax <- max(dsCry$CryingDuration)
-
-dsCryCeiling <- dsCry[dsCry$CryingDuration == cryMax, ]
-dsCryNotCeiling <- dsCry[dsCry$CryingDuration != cryMax, ]
-paletteCryHistogram <- c("#faa818", "#41a30d", "#ffce38", "#367d7d", "#d33502", "#6ebcbc", "#37526d") #http://colrd.com/image-dna/23521/
-purplish <- "#544A8C"
+  annotate(geom="text", x=rCrit[2]+.14, y=.8, label="alpha/2==phantom(0)", hjust=.5, vjust=-.05, parse=TRUE, color=PaletteCritical[2]) +
+  annotate(geom="text", x=rCrit[2]+.14, y=.8, label=".025", hjust=.5, vjust=1.05, parse=F, color=PaletteCritical[2]) +
+  annotate(geom="text", x=rCrit[2], y=0, label=round(rCrit[2],2), hjust=.5, vjust=1.2, parse=F, color=PaletteCritical[2], size=5) +
   
-cushion <- 3
-height1 <- 14.5
-height2 <- 13
-
-gCrying1 <- ggplot(dsCryNotCeiling, aes(x=CryingDuration)) +
-  geom_histogram(data=dsCryCeiling, binwidth=5, fill=paletteCryHistogram[3], color=paletteCryHistogram[1]) +
-  geom_histogram(binwidth=5, fill=paletteCryHistogram[6], color=paletteCryHistogram[4]) +
-  annotate("segment", x=cryMeanOverall, xend=cryMeanOverall, y=0, yend=Inf, color=paletteCryHistogram[7], size=3, alpha=1) +
-  annotate("segment", x=cryMeanControl, xend=cryMeanControl, y=0, yend=Inf, color=paletteCryHistogram[5], size=3, alpha=1, linetype="61") +
-#   annotate("segment", x=cryMax, xend=cryMax, y=0, yend=Inf, color=paletteCryHistogram[1], size=3, alpha=1, linetype="11") +
-#   geom_segment(aes(x=cryMeanOverall + cushion, y=height1, xend=cryMax - cushion, yend=height1), color=paletteCryHistogram[1], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#   geom_segment(aes(x=cryMax - cushion, y=height1, xend=cryMeanOverall + cushion, yend=height1), color=paletteCryHistogram[1], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#   geom_segment(aes(x=cryMeanOverall + cushion, y=height2, xend=cryMeanControl - cushion, yend=height2), color=paletteCryHistogram[5], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#   geom_segment(aes(x=cryMeanControl - cushion, y=height2, xend=cryMeanOverall + cushion, yend=height2), color=paletteCryHistogram[5], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#   geom_segment(aes(x=cryMeanControl + cushion, y=height2, xend=cryMax - cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#   geom_segment(aes(x=cryMax - cushion, y=height2, xend=cryMeanControl + cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-  annotate(geom="text", x=cryMeanOverall, y=Inf, label="Grand\nMean", hjust=.5, vjust=-.2, color=paletteCryHistogram[7], size=4, lineheight=.8) +
-  annotate(geom="text", x=cryMeanControl, y=Inf, label="Control Group\nMean", hjust=.5, vjust=-.2, color=paletteCryHistogram[5], size=4, lineheight=.8) +
-#   annotate(geom="text", x=cryMax, y=Inf, label="Baby Who\nCried Most", hjust=.5, vjust=-.1, color=paletteCryHistogram[1], size=4, lineheight=.8) +  
-  scale_y_continuous(limits=c(0, 15.2), expand=c(0,0)) +
-  chapterTheme +
-  labs(x="Crying Duration", y="Frequency", title="")
-
-DrawWithoutPanelClipping(gCrying1)
-#####################################
-## @knitr Figure12_11
-gCrying2 <- gCrying1 +
-  annotate("segment", x=cryMax, xend=cryMax, y=0, yend=Inf, color=paletteCryHistogram[1], size=3, alpha=1, linetype="11") +
-  geom_segment(aes(x=cryMeanOverall + cushion, y=height1, xend=cryMax - cushion, yend=height1), color=paletteCryHistogram[1], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-  geom_segment(aes(x=cryMax - cushion, y=height1, xend=cryMeanOverall + cushion, yend=height1), color=paletteCryHistogram[1], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#     geom_segment(aes(x=cryMeanOverall + cushion, y=height2, xend=cryMeanControl - cushion, yend=height2), color=paletteCryHistogram[5], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#     geom_segment(aes(x=cryMeanControl - cushion, y=height2, xend=cryMeanOverall + cushion, yend=height2), color=paletteCryHistogram[5], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#     geom_segment(aes(x=cryMeanControl + cushion, y=height2, xend=cryMax - cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#     geom_segment(aes(x=cryMax - cushion, y=height2, xend=cryMeanControl + cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round")
-  annotate(geom="text", x=cryMax, y=Inf, label="Baby Who\nCried Most", hjust=.5, vjust=-.1, color=paletteCryHistogram[1], size=4, lineheight=.8)
-
-DrawWithoutPanelClipping(gCrying2)
-#####################################
-## @knitr Figure12_12
-gCrying3 <- gCrying2 +
-  geom_segment(aes(x=cryMeanOverall + cushion, y=height2, xend=cryMeanControl - cushion, yend=height2), color=paletteCryHistogram[5], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-  geom_segment(aes(x=cryMeanControl - cushion, y=height2, xend=cryMeanOverall + cushion, yend=height2), color=paletteCryHistogram[5], size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round")
-#     geom_segment(aes(x=cryMeanControl + cushion, y=height2, xend=cryMax - cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-#     geom_segment(aes(x=cryMax - cushion, y=height2, xend=cryMeanControl + cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round")
-
-DrawWithoutPanelClipping(gCrying3)
-#####################################
-## @knitr Figure12_13
-gCrying4 <- gCrying3 +
-    geom_segment(aes(x=cryMeanControl + cushion, y=height2, xend=cryMax - cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round") +
-    geom_segment(aes(x=cryMax - cushion, y=height2, xend=cryMeanControl + cushion, yend=height2), color=purplish, size=2, arrow=arrow(length=grid::unit(0.3, "cm"), type="closed"), lineend="round")
-
-DrawWithoutPanelClipping(gCrying4)
-#####################################
-## @knitr Figure12_14
-
-f2_80 <- function( x ) { return( df(x, df1=2, df2=80) ) }
-criticalF05 <- qf(p=.95, df1=2, df2=80)
-
-grid.newpage()
-gCritical <- ggplot(data.frame(f=c(0, 4.5)), aes(x=f)) +
-  annotate("segment", x=criticalF05, xend=criticalF05, y=0, yend=Inf, color=PaletteCritical[2]) +
-  stat_function(fun=LimitRange(f2_80, criticalF05, Inf), geom="area", fill=PaletteCritical[2], alpha=1, n=calculatedPointCount) +
-  stat_function(fun=f2_80, n=calculatedPointCount, color=PaletteCritical[1], size=.5) +
-  annotate(geom="text", x=3.7, y=f2_80(criticalF05)+.15, label="alpha==phantom(0)", hjust=.5, vjust=-.15, parse=TRUE, color=PaletteCritical[2]) +
-  annotate(geom="text", x=3.7, y=f2_80(criticalF05)+.15, label=".05", hjust=.5, vjust=1.15, parse=F, color=PaletteCritical[2]) +
-  annotate(geom="text", x=criticalF05, y=0, label=round(criticalF05, 2), hjust=.5, vjust=1.2, color=PaletteCritical[2], size=5) +
-  scale_x_continuous(expand=c(0,0), breaks=0:4, labels=c(0, 1, 2, "", 4)) +
+  scale_x_continuous(expand=c(0,.01), breaks=c(-1, -.75, -.5, -.25, 0, .25, .5, .75, 1), labels=c(-1, -.75, -.5, "", 0, "", .5, .75, 1)) +
   scale_y_continuous(breaks=NULL, expand=c(0,0)) +
-  expand_limits(y=f2_80(.5) * 1.05) +
+  expand_limits(y=max(dsRho$PriorR) * 1.05) +
+  
   chapterTheme +
-  theme(axis.text = element_text(colour="gray60")) + #Lighten so the critical values aren't interfered with
-  labs(x=expression(italic(F)), y=NULL)
+  labs(x=expression(italic(R)), y=NULL)
 
-DrawWithoutPanelClipping(gCritical)
+DrawWithoutPanelClipping(gCriticalR)
---
+++ Chapter13/Chapter13.md
@@ -0,0 +1,96 @@
+Chapter 13 Graphs
+=================================================
+This report creates the chapter graphs.
+
+<!--  Set the working directory to the repository's base directory; this assumes the report is nested inside of only one directory.-->
+
+```r
+opts_knit$set(root.dir = "../")  #Don't combine this call with any other chunk -especially one that uses file paths.
+```
+
+
+<!-- Set the report-wide options, and point to the external code file. -->
+
+```r
+require(knitr)
+opts_chunk$set(
+    results='show', 
+    comment = NA, 
+    tidy = FALSE,
+    fig.width = 5.5, 
+    fig.height = 4, 
+    out.width = "600px", #This affects only the markdown, not the underlying png file.  The height will be scaled appropriately.
+    fig.path = 'figure_rmd/',     
+    dev = "png",
+    dpi = 400
+    #dev = "pdf"
+)
+echoChunks <- FALSE
+options(width=120) #So the output is 50% wider than the default.
+read_chunk("./Chapter13/Chapter13.R") 
+```
+
+<!-- Load the packages.  Suppress the output when loading packages. --> 
+
+
+
+<!-- Load any Global functions and variables declared in the R file.  Suppress the output. --> 
+
+
+
+<!-- Declare any global functions specific to a Rmd output.  Suppress the output. --> 
+
+
+
+<!-- Load the datasets. -->
+
+
+
+<!-- Tweak the datasets. -->
+
+
+
+## Figure 13-1
+<img src="figure_rmd/Figure13_01.png" title="plot of chunk Figure13_01" alt="plot of chunk Figure13_01" width="600px" />
+
+## Figure 13-2
+
+
+```
+Lise, would you like to keep the critical values, or hide them?
+```
+
+<img src="figure_rmd/Figure13_02.png" title="plot of chunk Figure13_02" alt="plot of chunk Figure13_02" width="600px" />
+
+
+
+
+## Session Info
+For the sake of documentation and reproducibility, the current report was build on a system using the following software.
+
+
+```
+Report created by Will at 2014-03-08, 16:45:29 -0600
+```
+
+```
+R Under development (unstable) (2014-03-07 r65143)
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+locale:
+[1] LC_COLLATE=English_United States.1252  LC_CTYPE=English_United States.1252    LC_MONETARY=English_United States.1252
+[4] LC_NUMERIC=C                           LC_TIME=English_United States.1252    
+
+attached base packages:
+[1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] RColorBrewer_1.0-5 dichromat_2.0-0    extrafont_0.16     ggplot2_0.9.3.1    scales_0.2.3       plyr_1.8.1        
+[7] knitr_1.5         
+
+loaded via a namespace (and not attached):
+ [1] colorspace_1.2-4 digest_0.6.4     evaluate_0.5.1   extrafontdb_1.0  formatR_0.10     gtable_0.1.2    
+ [7] labeling_0.2     MASS_7.3-29      munsell_0.4.2    proto_0.3-10     Rcpp_0.11.0      reshape2_1.2.2  
+[13] Rttf2pt1_1.2     stringr_0.6.2    tools_3.1.0     
+```
+

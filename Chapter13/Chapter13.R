bf7d6bfa2d897744869ca2aff4105f506ff39ece
Will Beasley <wibeasley@hotmail.com>
2014-03-10 21:19:10 -0500

    Changes after conversation with Lise

--- Chapter13/Chapter13.R
+++ Chapter13/Chapter13.R
@@ -37,7 +37,7 @@ paletteCryBox <- paletteCryBoxFull[c(6,5,3)]
 names(paletteCryBox) <- cryGroupLevels
 paletteCryBoxLight <- adjustcolor(paletteCryBox, alpha.f=.2)
 
-
+colorAxes <- "gray40"
 
 #####################################
 ## @knitr LoadDatasets
@@ -55,7 +55,7 @@ gObesity <- ggplot(dsObesity, aes(x=FoodHardshipRate, y=ObesityRate)) +
   geom_point(shape=21, size=3, color="aquamarine4", fill=adjustcolor("aquamarine4", alpha.f=.1)) + #This color should match the obesity Cleveland dot plot
   scale_x_continuous(label=scales::percent) +
   scale_y_continuous(label=scales::percent) +
-  coord_fixed() + 
+  coord_fixed(xlim=c(.09, .27), ylim=c(.19, .37)) + 
   chapterTheme +
   labs(x="Food Hardship Rate (in 2011)", y="Obesity Rate (in 2011)")
 
@@ -68,7 +68,8 @@ rPrior <- 0
 nPrior <- 51
 precPrior <- nPrior - 3
 zPrior <- atanh(rPrior)
-zCrit <- qnorm(p=c(.025, .975), mean=zPrior, sd=1/sqrt(precPrior))
+#zCrit <- qnorm(p=c(.025, .975), mean=zPrior, sd=1/sqrt(precPrior))
+zCrit <- qnorm(p=c(.95), mean=zPrior, sd=1/sqrt(precPrior))
 rCrit <- tanh(zCrit)
 
 Gauss <- function( fixed, rho, n ) {
@@ -81,25 +82,25 @@ Gauss <- function( fixed, rho, n ) {
 dsRho <- data.frame(ZPossible = seq(from=-3+intervalWidth, to=3-intervalWidth, by=intervalWidth))
 dsRho$RhoPossible <- tanh(dsRho$ZPossible)
 dsRho$PriorR <- Gauss(fixed=rPrior, rho=dsRho$RhoPossible, n=nPrior)
-dsRho$TailLower <- (dsRho$RhoPossible <= rCrit[1])
-dsRho$TailUpper <- (rCrit[2] <= dsRho$RhoPossible)
+# dsRho$TailLower <- (dsRho$RhoPossible <= rCrit[1])
+# dsRho$TailUpper <- (rCrit[2] <= dsRho$RhoPossible)
+dsRho$TailUpper <- (rCrit[1] <= dsRho$RhoPossible)
 
-cat("Lise, would you like to keep the critical values, or hide them?")
 gCriticalR <- ggplot(dsRho, aes(x=RhoPossible, y=PriorR)) + #, fill=TailLower
   annotate("segment", x=rCrit, xend=rCrit, y=0, yend=Inf, color=PaletteCritical[2]) +
   geom_area(data=dsRho[dsRho$TailLower, ], aes(x=RhoPossible, y=PriorR), fill=PaletteCritical[2]) +
   geom_area(data=dsRho[dsRho$TailUpper, ], aes(x=RhoPossible, y=PriorR), fill=PaletteCritical[2]) +
   geom_line(color=PaletteCritical[1]) +
   
-  annotate(geom="text", x=rCrit[1]-.09, y=.8, label="alpha/2==phantom(0)", hjust=.5, vjust=-.05, parse=TRUE, color=PaletteCritical[2]) +
-  annotate(geom="text", x=rCrit[1]-.09, y=.8, label=".025", hjust=.5, vjust=1.05, parse=F, color=PaletteCritical[2]) +
-  annotate(geom="text", x=rCrit[1], y=0, label=round(rCrit[1],2), hjust=.5, vjust=1.2, parse=F, color=PaletteCritical[2], size=5) +
+#   annotate(geom="text", x=rCrit[1]-.09, y=.8, label="alpha/2==phantom(0)", hjust=.5, vjust=-.05, parse=TRUE, color=PaletteCritical[2]) +
+#   annotate(geom="text", x=rCrit[1]-.09, y=.8, label=".025", hjust=.5, vjust=1.05, parse=F, color=PaletteCritical[2]) +
+#   annotate(geom="text", x=rCrit[1], y=0, label=round(rCrit[1],2), hjust=.5, vjust=1.2, parse=F, color=PaletteCritical[2], size=5) +
   
-  annotate(geom="text", x=rCrit[2]+.14, y=.8, label="alpha/2==phantom(0)", hjust=.5, vjust=-.05, parse=TRUE, color=PaletteCritical[2]) +
-  annotate(geom="text", x=rCrit[2]+.14, y=.8, label=".025", hjust=.5, vjust=1.05, parse=F, color=PaletteCritical[2]) +
-  annotate(geom="text", x=rCrit[2], y=0, label=round(rCrit[2],2), hjust=.5, vjust=1.2, parse=F, color=PaletteCritical[2], size=5) +
+  annotate(geom="text", x=rCrit[1]+.14, y=.8, label="alpha/2==phantom(0)", hjust=.5, vjust=-.05, parse=TRUE, color=PaletteCritical[2]) +
+  annotate(geom="text", x=rCrit[1]+.14, y=.8, label=".025", hjust=.5, vjust=1.05, parse=F, color=PaletteCritical[2]) +
+#   annotate(geom="text", x=rCrit[1], y=0, label=round(rCrit[1],2), hjust=.5, vjust=1.2, parse=F, color=PaletteCritical[2], size=5) +
   
-  scale_x_continuous(expand=c(0,.01), breaks=c(-1, -.75, -.5, -.25, 0, .25, .5, .75, 1), labels=c(-1, -.75, -.5, "", 0, "", .5, .75, 1)) +
+  scale_x_continuous(expand=c(0,.01), breaks=c(-1, -.75, -.5, -.25, 0, .25, .5, .75, 1)) + #, labels=c(-1, -.75, -.5, "", 0, "", .5, .75, 1)) +
   scale_y_continuous(breaks=NULL, expand=c(0,0)) +
   expand_limits(y=max(dsRho$PriorR) * 1.05) +
   
@@ -110,33 +111,29 @@ DrawWithoutPanelClipping(gCriticalR)
 
 #####################################
 ## @knitr Figure13_03
-require(MASS)
+# require(MASS)
 require(mnormt)
 
 palettePlacidSeas <- c("#fefefe","#1c5f83","#7ebea5","#f3d6a8","#3c765f","#5cbddd","#986a46") #http://colrd.com/image-dna/23557/
 palettePlacidSeasMedium <- grDevices::adjustcolor(palettePlacidSeas, alpha.f=.4)
 palettePlacidSeasLight <- grDevices::adjustcolor(palettePlacidSeas, alpha.f=.1)
-
-boundary <- 2.5
-points <- seq(-boundary, boundary, by=.2)
-dsMVGrid <- data.frame(
-  X = rep(points, each=length(points)),
-  Y = rep(points, times=length(points))
-)
-rObserved <- .6
-mu <- c(0,0)
-sigma <- matrix(c(1, rObserved, rObserved, 1), 2, 2)
-dsMVGrid$Z <- dmnorm(dsMVGrid, mu, sigma)
-zMatrix <- matrix(dsMVGrid$Z, nrow=length(points))
-zScale <- 10
-
 colorTheme <- "gray40"
+
 # open3d() # New window
-
-# rgl.viewpoint(theta=0, phi=-35)
-
-# rgl.viewpoint(theta=0,phi=-85)
-Graph3DMVNorm <- function( theta=0, phi=-35 ) {
+Graph3DMVNorm <- function( rho=0, theta=0, phi=-35 ) {
+  boundary <- 2.5
+  points <- seq(-boundary, boundary, by=.2)
+  dsMVGrid <- data.frame(
+    X = rep(points, each=length(points)),
+    Y = rep(points, times=length(points))
+  )
+#   rObserved <- .6
+  mu <- c(0,0)
+  sigma <- matrix(c(1, rho, rho, 1), 2, 2)
+  dsMVGrid$Z <- mnormt::dmnorm(dsMVGrid, mu, sigma)
+  zMatrix <- matrix(dsMVGrid$Z, nrow=length(points))
+  zScale <- 10
+  
   clear3d("all") # clear scene
   rgl.viewpoint(theta=theta, phi=phi, zoom=1) #A zoom smaller than one enlarges the graph's relative size.
   # bg3d(color="#887777")
@@ -146,25 +143,27 @@ Graph3DMVNorm <- function( theta=0, phi=-35 ) {
   axis3d("y-", col=colorTheme)
   rgl::mtext3d(text="X", edge="x-", labels=TRUE, line=2)
   rgl::mtext3d(text="Y", edge="y-", labels=TRUE, line=2)
-  rgl.lines(x=c(-boundary,boundary), y=c(0,0), z=c(0,0), color=palettePlacidSeas[5], lwd=3)
-  rgl.lines(x=c(0,0), y=c(-boundary,boundary), z=c(0,0), color=palettePlacidSeas[5], lwd=3)
+#   rgl.lines(x=c(-boundary,boundary), y=c(0,0), z=c(0,0), color=palettePlacidSeas[5], lwd=3)
+#   rgl.lines(x=c(0,0), y=c(-boundary,boundary), z=c(0,0), color=palettePlacidSeas[5], lwd=3)
 }
 Graph3DMVNorm()
 # Graph3DMVNorm(theta=5, phi=-85)
 
 #####################################
 ## @knitr Figure13_03b
-set.seed(3242) #Set seed so plots are consistent overtime.
-
-dsMV <- data.frame(MASS::mvrnorm(n=1000, mu=mu, Sigma=sigma))
-colnames(dsMV) <- c("X", "Y")
-ggplot(dsMV, aes(x=X, y=Y)) +
-  geom_hline(color=palettePlacidSeas[5], size=1) +
-  geom_vline(color=palettePlacidSeas[5], size=1) +
-  geom_point(shape=21, size=3, color=palettePlacidSeasMedium[3], fill=palettePlacidSeasLight[3]) +
-  coord_equal(xlim=c(-2.5, 2.5), ylim=c(-2.5, 2.5)) +  
-  chapterTheme +
-  labs(x=expression(italic(X)), y=expression(italic(Y)), parse=T)
+open3d() # New window
+Graph3DMVNorm(rho=0.6)
+# set.seed(3242) #Set seed so plots are consistent overtime.
+# 
+# dsMV <- data.frame(MASS::mvrnorm(n=1000, mu=mu, Sigma=sigma))
+# colnames(dsMV) <- c("X", "Y")
+# ggplot(dsMV, aes(x=X, y=Y)) +
+#   geom_hline(color=palettePlacidSeas[5], size=1) +
+#   geom_vline(color=palettePlacidSeas[5], size=1) +
+#   geom_point(shape=21, size=3, color=palettePlacidSeasMedium[3], fill=palettePlacidSeasLight[3]) +
+#   coord_equal(xlim=c(-2.5, 2.5), ylim=c(-2.5, 2.5)) +  
+#   chapterTheme +
+#   labs(x=expression(italic(X)), y=expression(italic(Y)), parse=T)
 
 #####################################
 ## @knitr Figure13_04
@@ -192,10 +191,11 @@ gStraightUp +
 
 #####################################
 ## @knitr Figure13_07
-cat("Lise, does this cross the y axis the way you'd like?")
 dsStairsDown <- data.frame(X=0:20, Y=20:0)
 
 gStraightDown <- ggplot(dsStairsDown, aes(x=X, y=Y)) +
+  geom_hline(color=colorAxes, size=1) +
+  geom_vline(color=colorAxes, size=1) +
   geom_abline(intercept=20, slope=-1, color=palettePlacidSeasMedium[3], size=1.5) +
   geom_point(shape=21, size=3, color=palettePlacidSeas[5], fill=palettePlacidSeasMedium[5]) +
   scale_y_continuous(labels=scales::dollar) +
@@ -216,10 +216,11 @@ gStraightDown +
 
 #####################################
 ## @knitr Figure13_10
-cat("Lise, I extended the plot so the -2 y intercept is visible.  Does this help?  Is the arrow too much?")
 dsNewspaperDelay <- data.frame(X=2:10, Y=0:8)
 
 ggplot(dsNewspaperDelay, aes(x=X, y=Y)) +
+  geom_hline() +
+  geom_vline() +
   geom_abline(intercept=-2, slope=1, color=palettePlacidSeasMedium[3], size=1.5) +
   geom_point(shape=21, size=3, color=palettePlacidSeas[5], fill=palettePlacidSeasMedium[5]) +
   annotate("segment", x=2, xend=.5, y=-2, yend=-2, arrow=grid::arrow(length=unit(.4,"cm")), color=palettePlacidSeas[2], size=2, lineend="round") +
@@ -236,6 +237,8 @@ dsBarb <- data.frame(X=seq(0, 8, by=2))
 dsBarb$Y <- 20 + (5 * dsBarb$X)
 
 ggplot(dsBarb, aes(x=X, y=Y)) +
+  geom_hline(color=colorAxes) +
+  geom_vline(color=colorAxes) +
   geom_abline(intercept=20, slope=5, color=palettePlacidSeasMedium[3], size=1.5) +
   geom_point(shape=21, size=3, color=palettePlacidSeas[5], fill=palettePlacidSeasMedium[5]) +
   scale_x_continuous(breaks=seq(0, 8, by=2)) +
@@ -245,29 +248,29 @@ ggplot(dsBarb, aes(x=X, y=Y)) +
   labs(x="Number of Days Out of Town", y="Amount Owed to Barb")
 
 #####################################
-## @knitr Figure13_13
-cat("Lise, this line truncates at the observed data points for two reasons.  First, it's a tad easier for me.  Second, I'm a little worried we'd be criticized for teaching them to extrapolate beyond the domain of the observed X values.")
-gObesityWithLine <- gObesity +
-  geom_smooth(method="lm", color=palettePlacidSeasMedium[3], size=1.5, se=F)
-gObesityWithLine
-#####################################
-## @knitr Figure13_14
+## @knitr Figure13_12
 fit <- lm(ObesityRate ~ 1 + FoodHardshipRate, data=dsObesity)
 xNew <- max(dsObesity$FoodHardshipRate) #.245
 yObs <- max(dsObesity$ObesityRate) #0.349
 yHat <- predict.lm(fit, data.frame(FoodHardshipRate=xNew))
 residual <- (yObs - yHat)
 
+gObesityWithLine <- gObesity +
+  geom_abline(intercept=coef(fit)["(Intercept)"], slope=coef(fit)["FoodHardshipRate"], color=palettePlacidSeasMedium[3], size=1.5)
+#   geom_smooth(method="lm", color=palettePlacidSeasMedium[3], size=1.5, se=F)
+gObesityWithLine
+#####################################
+## @knitr Figure13_13
 gObesityWithLine +
   annotate("segment", x=xNew, xend=xNew, y=yHat, yend=-Inf, color=palettePlacidSeasMedium[6], size=2, lineend="round") +
   annotate("segment", x=-Inf, xend=xNew, y=yHat, yend=yHat, color=palettePlacidSeasMedium[6], size=2, lineend="round")
 
 #####################################
-## @knitr Figure13_15
+## @knitr Figure13_14
 gObesityWithLine +
   annotate("segment", x=xNew, xend=xNew, y=yHat, yend=yObs, color=palettePlacidSeasMedium[7], size=1, lineend="round")
 
 #####################################
-## @knitr Figure13_16
+## @knitr Figure13_15
 gObesityWithLine +
   annotate("rect", xmin=xNew-residual, xmax=xNew, ymin=yHat, ymax=yObs, color=palettePlacidSeasMedium[7], fill=palettePlacidSeasLight[7], size=1, lineend="round")
